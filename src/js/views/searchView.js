b4662a102d5ae76db3899cc8b3c75ed720fcf686
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-02-20 01:04:42 +0100

    handing search event using Pub/Sub pattern

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,9 +1,18 @@
-class searchView {
-  #parentElement = document.querySelector(".form");
+class SearchView {
+  #parentElement = document.querySelector("form");
 
   getQuery() {
     const query = this.#parentElement.querySelector(".search__field").value;
+
+    return query;
+  }
+
+  addHandlerSearch(handler) {
+    this.#parentElement.addEventListener("submit", function (event) {
+      event.preventDefault();
+      handler();
+    });
   }
 }
 
-export default new searchView();
+export default new SearchView();
