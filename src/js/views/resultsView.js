009593bcabf46ef151cc823a088a774aca145571
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-02-21 01:31:13 +0100

    error message fixed

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -5,6 +5,8 @@ import View from "./View.js";
 
 class ResultsView extends View {
   _parentElement = document.querySelector(".results");
+  _errorMessage = "No recipes found for your query, please try again !!";
+  _message = "";
 
   _generateMarkup() {
     return this._data.map(this._generateMarkupPreview).join("");
