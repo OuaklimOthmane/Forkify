ff145adebad3f89d8ef442a82693c883ca3ed77e
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-02-21 01:32:43 +0100

    explaination added

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -17,7 +17,7 @@ export const getJSON = async function (url) {
 
     return data;
   } catch (err) {
-    //? We want to handle the errors in "model.js" not "getJson()",therefore we have to rethrow the error here, then the promise that will return from "getJson()" will be rejected, then we will be able to handle the error in "model.js", so besically the error propagates down from the async function "getJson()" to the other in "model.js" by rethrowing the error in the "catch" block.
+    //? We want to handle the errors in "model.js" not "getJson()",therefore we have to re-throw the error here, then the promise that will return from "getJson()" will be rejected, then we will be able to handle the error in "model.js", so besically the error propagates down from the async function "getJson()" to the other in "model.js" by re-throwing the error in the "catch" block.
     throw err;
   }
 };
